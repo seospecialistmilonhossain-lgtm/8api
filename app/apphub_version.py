494e808ace065c14b08ee27117dca94629ed396c
nyon <nilanoyon371@gmail.com>
2026-04-24 13:41:55 +0600

    fix 5

--- app/apphub_version.py
+++ app/apphub_version.py
@@ -2,7 +2,7 @@
 # Update this file when you release a new version of AppHub
 
 VERSION = "8.0.0"
-BUILD_NUMBER = 1
+BUILD_NUMBER = 8
 # Force all builds older than the current release to update.
 MIN_SUPPORTED_BUILD = BUILD_NUMBER
 RELEASE_DATE = "2026-04-23"
